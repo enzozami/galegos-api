83a7eb8995f16df2e3396299cac4e497eedcb4b3
Enzo Zamineli <enzozamineli@gmail.com>
2026-09-01 23:40:39 -0300

    feat: implement logout functionality in AuthController

--- domain/Models/Auth/Controllers/AuthController.php
+++ domain/Models/Auth/Controllers/AuthController.php
@@ -4,9 +4,12 @@ namespace Gal\Models\Auth\Controllers;
 
 use App\Http\Controllers\Controller;
 use Gal\Models\Auth\Actions\LoginAction;
+use Gal\Models\Auth\Actions\LogoutAction;
 use Gal\Models\Auth\DTOs\LoginDto;
 use Gal\Models\Auth\Requests\LoginRequest;
 use Gal\Models\User\Resource\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
@@ -15,5 +18,12 @@ class AuthController extends Controller
         $user = $action->handle(LoginDto::fromRequest($request), $request);
 
         return new UserResource($user);
+    }
+
+    public function logout(Request $request, LogoutAction $action): Response
+    {
+        $action->handle($request);
+
+        return response()->noContent();
     }
 }
