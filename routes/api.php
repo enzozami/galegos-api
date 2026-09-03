f55c784ade38c2fd27e31a2b4fa6ced8ff08cb1e
Enzo Zamineli <enzozamineli@gmail.com>
2026-09-02 22:02:14 -0300

    feat: add logout route to AuthController for user session management

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', [AuthController::class, 'login'])->name('login')->middleware(['web', 'guest']);
+
+Route::prefix('auth')->name('auth.')->middleware(['web', 'auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
