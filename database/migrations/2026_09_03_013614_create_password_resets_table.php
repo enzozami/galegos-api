6092373141e23a1c9ca449996161420a5e645d4a
Enzo Zamineli <enzozamineli@gmail.com>
2026-09-03 00:56:20 -0300

    feat: create migration for password_resets table to handle password reset functionality

---
+++ database/migrations/2026_09_03_013614_create_password_resets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('code');
+            $table->string('reset_token');
+            $table->unsignedTinyInteger('attempts')->default(0);
+            $table->timestamp('verified_at')->nullable();
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('password_resets');
+    }
+};
