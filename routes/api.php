2c1448a287014f38170f99087ba2054d8462908e
Enzo Zamineli <enzozamineli@gmail.com>
2026-09-03 01:00:49 -0300

    feat: add authentication routes for forgot password, verify code, and reset password

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', [AuthController::class, 'login'])->name('login')->middleware(['web', 'guest']);
 
+Route::prefix('auth')->name('auth.')->middleware(['web', 'guest'])->group(function () {
+    Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
+    Route::post('verify-code', [AuthController::class, 'verifyCode'])->name('verify-code');
+    Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
+});
+
 Route::prefix('auth')->name('auth.')->middleware(['web', 'auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
