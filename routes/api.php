133f497834c985484b3d7592103b50158b64e5f6
Enzo Zamineli <enzozamineli@gmail.com>
2026-09-08 23:05:21 -0300

    feat: add registration route and update login route in auth group

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Gal\Models\Auth\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/login', [AuthController::class, 'login'])->name('login')->middleware(['web', 'guest']);
-
 Route::prefix('auth')->name('auth.')->middleware(['web', 'guest'])->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');;
+    Route::post('register', [AuthController::class, 'register'])->name('register');
     Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
     Route::post('verify-code', [AuthController::class, 'verifyCode'])->name('verify-code');
     Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
